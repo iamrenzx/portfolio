04d3c1f6dec8ce5aff62ab27b8a9e512e07298d0
unknown <iamrenzx@gmail.com>
2021-06-11 02:24:07 +0800

    Add projects

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,19 +1,23 @@
 /* eslint-disable jsx-a11y/mouse-events-have-key-events */
-import React, { useState } from 'react';
+import React, { useRef, useState, MutableRefObject } from 'react';
+import Projects from '../projects';
 
 const Home: React.FC = () => {
-  const [arrowPosition, setArrowPosition] = useState(false);
+  const [arrowPosition, setArrowPosition] = useState(true);
+  const myRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const executeScroll = () => myRef.current.scrollIntoView();
   return (
     <div className="container-fluid vh-100">
       <div className="row h-100 justify-content-center bg-home align-items-center">
         <div className="col-12 text-center">
-          <span className="h3 text-light-primary">Hello,</span>
+          <span className="h3 text-light-primary">Hello, </span>
           <span className="text-primary h3">I&apos;m Renz Delicano.</span>
           <br />
           <span className="h3 text-light-primary">I&apos;m a frontend developer.</span>
           <br />
           <button
             type="button"
+            onClick={executeScroll}
             className="mt-3 btn btn-outline-secondary btn-lg"
             onMouseOver={() => setArrowPosition(false)}
             onMouseLeave={() => setArrowPosition(true)}
@@ -23,6 +27,9 @@ const Home: React.FC = () => {
             <i className={`fa ${arrowPosition ? 'fa-arrow-right' : 'fa-arrow-down'} `} />
           </button>
         </div>
+      </div>
+      <div className="row justify-content-center h-100" ref={myRef}>
+        <Projects />
       </div>
     </div>
   );
---
+++ src/projects/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import project1 from '../images/project1.png';
+
+const Projects: React.FC = () => {
+  const arrayOfProjects = [
+    {
+      image: project1,
+      title: 'MyEntrego',
+      technologies: ['React JS', 'Ant Design', 'Sass', 'Redux'],
+      url: 'https://my.entrego.com.ph/',
+    },
+  ];
+
+  return (
+    <div className="col-12 mt-5">
+      <div className="row">
+        <div className="col-12">
+          <h4 className="text-center mb-0">PROJECTS</h4>
+          <h2 className="text-center font-weight-bold">____</h2>
+        </div>
+      </div>
+      <div className="row justify-content-center mt-5 mx-lg-5">
+        {arrayOfProjects.map((v) => (
+          <div className="col-md-4 h-100" key={v.title + v.technologies.length}>
+            <div className="card">
+              <img className="card-img-top" src={v.image} alt="Card cap1" />
+              <div className="card-body">
+                <p className="card-text fw-bold">{v.title}</p>
+                <p className="card-text">
+                  Technologies Used:
+                  <span className="fst-italic">{v.technologies.map((t, index) => (` ${t} ${index === v.technologies.length - 1 ? '' : '/'}`))}</span>
+                </p>
+                <a href={v.url} className="btn btn-outline-secondary w-100 stretched-link text-decoration-none" target="_blank" rel="noreferrer">
+                  View Page
+                  {' '}
+                  <i className="fa fa-eye" />
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
